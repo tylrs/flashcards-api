c43dd2b5c79fe8010077cce8c82a8c312ceb46a6
Taylor Galloway <galloway.taylor@gmail.com>
2023-11-28 18:25:27 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Clear existing data
+Folder.destroy_all
+Flashcard.destroy_all
+
+# Create folders
+folders = Folder.create([
+  { 
+    id: 1,
+    name: "Mathematics" 
+  },
+  { 
+    id: 2,
+    name: "Science" 
+  },
+  { 
+    id: 3,
+    name: "Languages"
+   }
+])
+
+# Create flashcards
+flashcards = Flashcard.create([
+  { folder_id: folders[0][:id], front: "2 + 2", back: "4" },
+  { folder_id: folders[0][:id], front: "5 * 5", back: "25" },
+  { folder_id: folders[1][:id], front: "Chemical symbol for water", back: "H2O" },
+  { folder_id: folders[1][:id], front: "Newton\'s First Law", back: "An object at rest stays at rest and an object in motion stays in motion with the same speed and in the same direction unless acted upon by an unbalanced external force." },
+  { folder_id: folders[2][:id], front: "Spanish for 'Hello'", back: "Hola" },
+  { folder_id: folders[2][:id], front: "French for 'Goodbye'", back: "Au revoir" }
+])
+
+puts 'Seed data has been created!'
