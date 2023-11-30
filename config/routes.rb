aad6ffa3d0b568196b967c07c448e51eef473faa
Taylor Galloway <galloway.taylor@gmail.com>
2023-11-30 17:17:56 -0600

    Switch to single quotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  get "decks", to: "decks#index"
+  get 'decks', to: 'decks#index'
+  get 'decks/:deckId/flashcards', to: 'flashcards#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,26 @@ Flashcard.destroy_all
 decks = Deck.create([
   { 
     id: 1,
-    name: "Mathematics" 
+    name: 'Mathematics' 
   },
   { 
     id: 2,
-    name: "Science" 
+    name: 'Science' 
   },
   { 
     id: 3,
-    name: "Languages"
+    name: 'Languages'
    }
 ])
 
 # Create flashcards
 flashcards = Flashcard.create([
-  { deck_id: decks[0][:id], front: "2 + 2", back: "4" },
-  { deck_id: decks[0][:id], front: "5 * 5", back: "25" },
-  { deck_id: decks[1][:id], front: "Chemical symbol for water", back: "H2O" },
-  { deck_id: decks[1][:id], front: "Newton\'s First Law", back: "An object at rest stays at rest and an object in motion stays in motion with the same speed and in the same direction unless acted upon by an unbalanced external force." },
-  { deck_id: decks[2][:id], front: "Spanish for 'Hello'", back: "Hola" },
-  { deck_id: decks[2][:id], front: "French for 'Goodbye'", back: "Au revoir" }
+  { deck_id: decks[0][:id], front: '2 + 2', back: '4' },
+  { deck_id: decks[0][:id], front: '5 * 5', back: '25' },
+  { deck_id: decks[1][:id], front: 'Chemical symbol for water', back: 'H2O' },
+  { deck_id: decks[1][:id], front: 'Newton\'s First Law', back: 'An object at rest stays at rest and an object in motion stays in motion with the same speed and in the same direction unless acted upon by an unbalanced external force.' },
+  { deck_id: decks[2][:id], front: "Spanish for 'Hello'", back: 'Hola' },
+  { deck_id: decks[2][:id], front: "French for 'Goodbye'", back: 'Au revoir' }
 ])
 
 puts 'Seed data has been created!'
